09a198ab605cec7ca396a131f0f1d5243d78336e
Rafał Chomczyk <rchomczyk@icloud.com>
2023-05-19 15:05:10 +0200

    Use single webhook by aggregating awaiting notifications to reduce the number of outgoing requests

--- src/client/notification/StormAlertNotifier.ts
+++ src/client/notification/StormAlertNotifier.ts
@@ -1,9 +1,9 @@
+import { StormAlert, StormAlertZone } from "../storm/StormAlert";
 import { fetchStormAlerts } from "../storm/StormAlertController";
 import { EmbedMessage, EmbedField, EmbedThumbnail, EmbedFooter } from "./discord/component/EmbedMessage";
 import { DiscordClient } from "./discord/DiscordClient";
 import { Webhook } from "./discord/webhook/Webhook";
 import moment from "moment";
-import { StormAlert, StormAlertZone } from "../storm/StormAlert";
 
 export class StormAlertNotifier {
 
@@ -19,25 +19,35 @@ export class StormAlertNotifier {
 
     async notify(): Promise<void> {
         await fetchStormAlerts()
-            .then(alerts => alerts.forEach(async (alert) => {
-                const location = alert.scope
-                const missions = alert.missions.filter(mission => this.matchers.some(matcher => mission.rewards.includes(matcher)))
-                if (missions.length > 0) {
-                    await this.client.sendWebhook(this.target, this.buildNotification(location, missions))
+            .then(async (alerts) => {
+                const notifications = []
+
+                for (const alert of alerts) {
+                    const location = alert.scope
+                    const missions = alert.missions.filter(mission => this.matchers.some(matcher => mission.rewards.includes(matcher)))
+                    if (missions.length > 0) {
+                        notifications.push(this.getMessageWithAlerts(location, missions))
+                    }
                 }
-            }))
+
+                if (notifications.length > 0) {
+                    await this.client.sendWebhook(this.target, this.getWebhook(notifications))
+                }
+            })
     }
 
-    private buildNotification(location: StormAlertZone, missions: StormAlert[]): Webhook {
-        const prettyDate = moment(Date.now()).format("DD/MM/YYYY")
-        return new Webhook("stw-assistant", [
-            new EmbedMessage(
-                `Mission alerts in ${location.name} for ${prettyDate}`,
-                15856929,
-                missions.map(mission => new EmbedField(`${mission.name} [🗲 ${mission.power_level}]`, mission.rewards, false)),
-                "A spicy dose of new missions with pretty good rewards.",
-                moment().toISOString(true),
-                new EmbedThumbnail(location.icon),
-                new EmbedFooter("stw-assistant fetches data from Free The V-Bucks ❤", this.iconUrl)) ], this.iconUrl)
+    private getWebhook(components: Array<EmbedMessage>) {
+        return new Webhook("stw-assistant", components, this.iconUrl)
+    }
+
+    private getMessageWithAlerts(location: StormAlertZone, missions: Array<StormAlert>): EmbedMessage {
+        return new EmbedMessage(
+            `Mission alerts in ${location.name} for ${moment(Date.now()).format("DD/MM/YYYY")}`,
+            15856929,
+            missions.map(mission => new EmbedField(`${mission.name} [🗲 ${mission.power_level}]`, mission.rewards, false)),
+            "A spicy dose of new missions with pretty good rewards.",
+            moment().toISOString(true),
+            new EmbedThumbnail(location.icon),
+            new EmbedFooter("stw-assistant fetches data from Free The V-Bucks ❤", this.iconUrl))
     }
 }
